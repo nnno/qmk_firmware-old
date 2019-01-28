ec110bdf43af2c95874156be33def5493b7df337
nnno <nnno@nnno.jp>
2019-01-29 00:31:41 +0900

    日本語切替方法を変更

--- keyboards/ergodash/rev2/keymaps/nnno/keymap.c
+++ keyboards/ergodash/rev2/keymaps/nnno/keymap.c
@@ -37,11 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV  ,                        KC_DEL , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_QUOT ,                        KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LANG2,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER   ,                        RAISE  , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LALT, KC_LGUI, KC_LANG1,         KC_SPC,  LOWER   ,_______,        _______,RAISE  , KC_ENT,           KC_LEFT, KC_LBRC, KC_RBRC, KC_RGHT  \
+    KC_ESC , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV  ,                        KC_DEL  , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+    KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_QUOT ,                        KC_MINS , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    LOWER   ,                        KC_EQL  , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LANG2,                        KC_LANG1, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    RAISE  , KC_LALT, KC_LGUI, EISU,             KC_SPC,  KC_LANG2,_______,        _______,KC_LANG1, KC_ENT,           KC_LEFT, KC_LBRC, KC_RBRC, KC_RGHT  \
   ),
 
   /* Lower
