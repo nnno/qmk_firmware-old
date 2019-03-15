87341a7075f1fb70f343c5e0a7fb0caa103398d3
nnno <nnno@nnno.jp>
2019-03-16 00:50:49 +0900

    ergodashのキーマップをlily58のそれと同じ操作性にした

--- keyboards/ergodash/rev2/keymaps/nnno/keymap.c
+++ keyboards/ergodash/rev2/keymaps/nnno/keymap.c
@@ -25,23 +25,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * | ESC  |   1  |   2  |   3  |   4  |   5  |   [  |                    |   ]  |   6  |   7  |   8  |   9  |   0  |Pscree|
+   * | ESC  |   1  |   2  |   3  |   4  |   5  | ____ |                    | ____ |   6  |   7  |   8  |   9  |   0  | BSPC |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |  `   |   Q  |   W  |   E  |   R  |   T  |   -  |                    |   =  |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  | ____ |                    | ____ |   Y  |   U  |   I  |   O  |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   A  |   S  |   D  |   F  |   G  |   =  |                    |   -  |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |   =  |                    |   -  |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Lower|                    | Raise|   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | EISU |||||||| Space| Lower|      ||||||||      | Raise| Enter|||||||| Left | Down |  Up  | Right|
+   * | Down |  Up  |  ALt | LGUI |||||||| Space| Lower|      ||||||||      | Raise| Enter|||||||| Left | Down |  Up  | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
-    KC_ESC , KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV  ,                        KC_DEL  , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-    KC_TAB , KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_QUOT ,                        KC_MINS , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_EQL  ,                        KC_MINS , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER   ,                        RAISE   , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    RAISE  , KC_LALT, KC_LGUI, EISU,             KC_SPC,  LOWER   ,_______,        _______,RAISE   , KC_ENT,           KC_LEFT, KC_LBRC, KC_RBRC, KC_RGHT  \
+    KC_ESC , KC_1,     KC_2,    KC_3,    KC_4,    KC_5,  _______,                        _______,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC, \
+    KC_TAB , KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,  _______,                        _______,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS, \
+    KC_LCTL, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,   KC_EQL,                        KC_MINS,   KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT, \
+    KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    LOWER,                          RAISE,   KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, \
+    KC_DOWN, KC_UP, KC_LALT, KC_LGUI,           KC_SPC,    LOWER,_______,        _______,  RAISE, KC_ENT,           KC_GRV, KC_QUOT, KC_LEFT, KC_RGHT  \
   ),
 
   /* Lower
